552a187851a392459f6088ec44a3ed82dc152c09
logm <luiz-augusto.gomes@gft.com>
2022-08-10 16:43:56 -0300

    criando classe UserDTO

---
+++ src/main/java/com/dio/livecoding/springboot/dtos/UserDTO.java
@@ -0,0 +1,18 @@
+package com.dio.livecoding.springboot.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String name;
+
+    private String email;
+
+}
